b751cc01e8472ddd325d6ba597b9c8bbc025fd3e
FlorenceBrassard <florence.brassard@umontreal.ca>
2025-04-18 16:14:32 +0200

    Suppression complète

--- Pages encodées/variables.exemples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Fichier de variables (ignoré par Git)
-.
-# Chemin absolu vers l'exécutable de Saxon, à renseigner
-# ex. /Applications/Saxon/saxon.jar
-SAXON_PATH=""
-# Fichier .xsl, à renseigner
-XSL_PATH=""
-
